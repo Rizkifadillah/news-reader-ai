95944a21bebbe539dbeca987891fed8bfa8c0d42
RizkiFadillah <rizkifadillah26.rf@gmail.com>
2022-10-10 09:10:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { useEffect, useState } from 'react';
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './style.js';
+import {Typography} from '@material-ui/core'
+
+
+const alanKey = '6a56a6f5e810a8ef859adf5ba32e41142e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
+
+  const [newsArticles, setNewsArticles] = useState([])
+  const [activeArticle , setActiveArticle]=useState(-1);
+  const classes = useStyles();
+  useEffect(()=>{
+    alanBtn({
+      key: alanKey,
+      onCommand: ({command, articles}) => {
+        if(command === 'newHeadlines'){
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        }else if(command==='highlight'){
+          setActiveArticle((prevActiveArticle) => prevActiveArticle+1);
+        }
+      }
+
+    })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography className={classes.title} variant="h3"><b>News Reader Artificial Intelligence</b></Typography>
+
+      {/* <h3 className={classes.title}>News Reader Artificial IntelligenceI</h3> */}
+      <div className={classes.logoContainer}>
+        <img  src="https://th.bing.com/th/id/OIP.hpfA41UxWYp7BleWA_AoDAHaDt?pid=ImgDet&rs=1" className={classes.alanLogo} alt="alan logo"/>
+      </div>
+        <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
